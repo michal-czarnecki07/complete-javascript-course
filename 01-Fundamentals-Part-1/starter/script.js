d1182213b096d8a3ecdcfc938f23d66fcd07fb8c
michal-czarnecki07 <miccza@gmail.com>
2023-02-15 13:54:41 +0100

    zad1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+let js = 'amazing';
+console.log(40+20);
+
+
+let country;
+let continent;
+let population;
+country = "Poland";
+continent = "Europe";
+population = 38000000;
+console.log("Country: ", country)
