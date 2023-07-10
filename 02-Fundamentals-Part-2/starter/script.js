9e9efe65895e5f568787a95afa0f9adb39fb3888
Michal Czarnecki <miccza@gmail.com>
2023-07-10 21:53:31 +0200

    dev chapter

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,6 @@
 console.log("cos")
+const vovels = ['a', 'A', 'o', 'O', 'i', 'I', 'e', 'E'];
+str = "pooolskaaA";
+str = str.split("").filter(element => !vovels.includes(element)).join("");
+
+console.log(str);
